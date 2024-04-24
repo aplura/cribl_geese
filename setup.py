446bb468d690fbf2cfc9e8e50a103c43d5e22518
Kyle Smith <ksmith@aplura.com>
2024-04-24 18:37:48 -0400

    Bump version: 1.1.1 → 1.1.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setuptools.setup(
     name="geese",
-    version="1.1.1",
+    version="1.1.2",
     author="Aplura, LLC",
     author_email="appsupport@aplura.com",
     description="Cribl Migrator",
