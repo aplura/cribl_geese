09f055263a767639db4f6d69a9bf7f3d96a8ed5f
Kyle Smith <ksmith@aplura.com>
2024-06-06 18:31:28 -0400

    Thu Jun  6 18:31:28 EDT 2024 Updating Pytests

--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,10 @@
 import sys
-sys.path.append('../')
-import geese
+import os
+sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
+from geese import version
 
 
 class TestStatic:
 
     def test_version(self):
-        assert geese.version.__version__ == '1.1.2'
+        assert version.__version__ == '1.1.2'
