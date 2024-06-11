67cc52bc68f14d923c5d6d252c90ad415802fbe4
Kyle Smith <ksmith@aplura.com>
2024-06-11 08:00:21 -0700

    Tue Jun 11 08:00:21 PDT 2024 Total Test Removal, not just comment out

--- tests/tests.py
+++ tests/tests.py
@@ -9,18 +9,3 @@ class TestStatic:
 
     def test_version(self):
         assert version.__version__ == '1.1.3'
-
-    # def test_auth_env(self):
-    #     USERNAME = "GOATS"
-    #     TEST_KEY = "TEST_GOATS"
-    #     os.environ[TEST_KEY] = USERNAME
-    #     authObj = Authentication({TEST_KEY: TEST_KEY})
-    #     username_test = authObj._get_auth_env(TEST_KEY)
-    #     assert username_test == USERNAME
-    #
-    # def test_auth_file(self):
-    #     USERNAME = "GOATS"
-    #     TEST_KEY = "TEST_GOATS"
-    #     authObj = Authentication({TEST_KEY: USERNAME}, TEST_KEY)
-    #     username_test = authObj._get_auth_env(TEST_KEY)
-    #     assert username_test == USERNAME
