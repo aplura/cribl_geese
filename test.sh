18efd48c2f1a91e6b23121a1245909c8e8a03de7
Kyle Smith <ksmith@aplura.com>
2025-02-20 18:03:47 -0500

    Thu Feb 20 18:03:47 EST 2025 Export working as intended

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-geese export --export-dir cribl/tst
-geese export --export-dir cribl/tst_ns --use-namespace
-geese export --export-dir cribl/tst_ns_split --use-namespace --export-split
-geese export --export-dir cribl/tst_split --export-split
+geese export --all-objects --export-dir cribl/tst
+geese export --all-objects --export-dir cribl/tst_ns --use-namespace
+geese export --all-objects --export-dir cribl/tst_ns_split --use-namespace --export-split
+geese export --all-objects --export-dir cribl/tst_split --export-split
